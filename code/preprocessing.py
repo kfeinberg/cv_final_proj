fc4ab60cf1f620a12215e75697516b987d9ad12c
Kalissa Feinberg <kfeinberg@wustl.edu>
2022-04-25 15:55:33 -0400

    updated

--- code/preprocessing.py
+++ code/preprocessing.py
@@ -1,16 +1,20 @@
 # source: https://medium.com/@iKhushPatel/convert-video-to-images-images-to-video-using-opencv-python-db27a128a481
-
 import cv2
-vidcap = cv2.VideoCapture('/Users/kallifeinberg/Documents/Senior/Semester2/CS1430/cv_final_proj/data/video/bff.MOV')
+
+video_url = '/Users/kallifeinberg/Documents/Senior/Semester2/CS1430/cv_final_proj/data/video/vid1.mp4'
+directory_name = '5mvid1'
+sec = 0
+frameRate = 1 # it will capture images every 0.5 seconds
+count = 1
+
+vidcap = cv2.VideoCapture(video_url)
 def getFrame(sec):
     vidcap.set(cv2.CAP_PROP_POS_MSEC,sec*1000)
     hasFrames,image = vidcap.read()
     if hasFrames:
-        cv2.imwrite("image"+str(count)+".jpg", image)     # save frame as JPG file
+        cv2.imwrite("../data/images/image" + directory_name + str(count)+".jpg", image)     # save frame as JPG file
     return hasFrames
-sec = 0
-frameRate = 1 #//it will capture image in each 0.5 second
-count=1
+
 success = getFrame(sec)
 while success:
     count = count + 1
