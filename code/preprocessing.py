506c8f3e88948463017e2baf5331918d34caed99
Kalissa Feinberg <kfeinberg@wustl.edu>
2022-04-26 17:17:54 -0400

    prepreocessing with directory

--- code/preprocessing.py
+++ code/preprocessing.py
@@ -1,18 +1,20 @@
 # source: https://medium.com/@iKhushPatel/convert-video-to-images-images-to-video-using-opencv-python-db27a128a481
 import cv2
+import os
 
 video_url = '/Users/kallifeinberg/Documents/Senior/Semester2/CS1430/cv_final_proj/data/video/vid1.mp4'
 directory_name = '5mvid1'
 sec = 0
-frameRate = 1 # it will capture images every 0.5 seconds
+frameRate = 0.1 # it will capture images every 0.5 seconds
 count = 1
 
 vidcap = cv2.VideoCapture(video_url)
+os.mkdir("../data/images/" + directory_name)
 def getFrame(sec):
     vidcap.set(cv2.CAP_PROP_POS_MSEC,sec*1000)
     hasFrames,image = vidcap.read()
     if hasFrames:
-        cv2.imwrite("../data/images/image" + directory_name + str(count)+".jpg", image)     # save frame as JPG file
+        cv2.imwrite("../data/images/" + directory_name + "/" + str(count)+".jpg", image)     # save frame as JPG file
     return hasFrames
 
 success = getFrame(sec)
