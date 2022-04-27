e87cdc0334d85797d65fd755e1a53afdd71e222d
Kalissa Feinberg <kfeinberg@wustl.edu>
2022-04-27 13:00:57 -0400

    updated with images

--- code/preprocessing.py
+++ code/preprocessing.py
@@ -2,8 +2,8 @@
 import cv2
 import os
 
-video_url = '/Users/kallifeinberg/Documents/Senior/Semester2/CS1430/cv_final_proj/data/video/vid1.mp4'
-directory_name = '5mvid1'
+video_url = '/Users/kallifeinberg/Documents/Senior/Semester2/CS1430/cv_final_proj/data/video/vid2.mp4'
+directory_name = '5mvid2'
 sec = 0
 frameRate = 0.1 # it will capture images every 0.5 seconds
 count = 1
