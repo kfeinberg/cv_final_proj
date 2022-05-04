0212570e66044cbdf2c0bd55cbd562a5b012d0e8
Kalissa Feinberg <kfeinberg@wustl.edu>
2022-05-04 16:55:06 -0400

    more images preprocessed

--- code/preprocessing.py
+++ code/preprocessing.py
@@ -2,24 +2,33 @@
 import cv2
 import os
 
-video_url = '/Users/kallifeinberg/Documents/Senior/Semester2/CS1430/cv_final_proj/data/video/10mvid2.mp4'
-directory_name = '10mvid2'
-sec = 0
-frameRate = 0.1 # it will capture images every 0.5 seconds
-count = 1
+frameRate = 0.05 # it will capture images every 0.05 seconds
+directory = '/Users/kallifeinberg/Documents/Senior/Semester2/CS1430/cv_final_proj/data/video' # iterates through every file in this directory
 
-vidcap = cv2.VideoCapture(video_url)
-os.mkdir("../data/images/" + directory_name)
-def get_frame(sec):
+def get_frame(sec, vidcap, directory_name):
     vidcap.set(cv2.CAP_PROP_POS_MSEC,sec*1000)
     hasFrames,image = vidcap.read()
     if hasFrames:
         cv2.imwrite("../data/images/" + directory_name + "/" + str(count)+".jpg", image)     # save frame as JPG file
     return hasFrames
 
-success = get_frame(sec)
-while success:
-    count = count + 1
-    sec = sec + frameRate
-    sec = round(sec, 2)
-    success = get_frame(sec)
+
+for file in os.listdir(directory):
+    count = 1
+    sec = 0
+
+    file_name = os.fsdecode(file)
+    video_url = directory + "/" + file_name  
+
+    vidcap = cv2.VideoCapture(video_url)
+
+    if (file_name != ".DS_Store"):
+        directory_name = file_name.split('.')[0]
+        os.mkdir("../data/images/" + directory_name)
+
+        success = get_frame(sec, vidcap, directory_name)
+        while success:
+            count = count + 1
+            sec = sec + frameRate
+            sec = round(sec, 2)
+            success = get_frame(sec, vidcap, directory_name)
