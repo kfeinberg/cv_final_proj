6dddb3dea2567501e64af875cd62cb82c6437b86
michael-tap-ia <michael_tapia@brown.edu>
2022-05-09 21:39:50 -0400

    push data

--- code/data_split.py
+++ code/data_split.py
@@ -7,6 +7,7 @@ import os
 import numpy as np
 import os
 from check_label import *
+import xml.etree.ElementTree
 
 
 def split_three(lst, ratio=[0.8, 0.1, 0.1]):
@@ -23,6 +24,8 @@ def img_copy(dirs: list, destination: str, folder: str):
     file_cnt = 0
 
     for dir in dirs:       
+        print(f"Directory: {dir}")
+        
         # getting all the files in the source directory
         files = os.listdir(dir)
         subdir_ind = dir.rfind("/")
@@ -38,27 +41,45 @@ def img_copy(dirs: list, destination: str, folder: str):
 
         #rename all the files in this dir with folder name prefix
         for file in files:
-            new_file = destination + "/" + folder + "/" + subdir + "_" + file
+            new_file = destination + "/" + folder + "/" + str(subdir) + "_" + file
             if not os.path.exists(new_file): #if file already exist, remove it (due to windows overwrite issue with 'rename')
-                os.rename(destination + "/" + folder + "/" + subdir + "/" + file, new_file) #add clean
+                os.rename(destination + "/" + folder + "/" + str(subdir) + "/" + file, new_file) #add clean
+                print(f"Renamed {new_file}")
             else:
+            #    print(new_file)
                 os.remove(new_file) #remove
-                os.rename(destination + "/" + folder + "/" + subdir + "/" + file, new_file) #add after remove
-
+                os.rename(destination + "/" + folder + "/" + str(subdir) + "/" + file, new_file) #add after remove
+            ####### up until here the code is doing what is expected######
+            
             if file.endswith('.xml'):
-                shutil.move(destination + "/" + folder + "/" + subdir + "_" + file, 
-                                destination + "/" + folder + "/" + "annotations" + "/" + subdir + "_" + file)
+                #shutil.move(destination + "/" + folder + "/" + str(subdir) + "_" + file, 
+                #                destination + "/" + folder + "/" + "annotations" + "/" + file) #not sure why this name gets changed 
+                os.replace(new_file, destination + "/" + folder + "/" + "annotations" + "/" + str(subdir)+ "_" + file)
+                #Now we have to change where the XML is directing the address for the file:
+                # Open original file
+                et = xml.etree.ElementTree.parse(destination + "/" + folder + "/" + "annotations" + "/" + str(subdir)+ "_" + file)
+                xml_folder = et.find('folder')
+                xml_filename = et.find('filename')
+
+                xml_filename.text = xml_folder.text + "_" + xml_filename.text
+                # Write back to file
+                et.write(destination + "/" + folder + "/" + "annotations" + "/" + str(subdir)+ "_" + file)
+
+                #print(f"XML moved from {destination}/{folder}/{subdir}/{file} to \n {new_file}")
             elif file.endswith('.jpg'):
-                shutil.move(destination + "/" + folder + "/" + subdir + "_" + file, 
-                                destination + "/" + folder + "/" + "images" + "/" + subdir + "_" + file)
+                #shutil.move(destination + "/" + folder + "/" + str(subdir) + "_" + file, 
+                #                destination + "/" + folder + "/" + "images" + "/" + subdir + "_" + file)
+                os.replace(new_file, destination + "/" + folder + "/" + "images" + "/" + str(subdir)+ "_" + file)
             else:
-                shutil.move(destination + "/" + folder + "/" + subdir + "_" + file, 
-                                destination + "/" + folder + "/" + "check" + "/" + subdir + "_" + file)
+                #shutil.move(destination + "/" + folder + "/" + subdir + "_" + file, 
+                #                destination + "/" + folder + "/" + "check" + "/" + subdir + "_" + file)
+                os.replace(new_file, destination + "/" + folder + "/" + "check" + "/" + str(subdir)+ "_" + file)
             
             file_cnt += 1
         os.rmdir(destination + "/" + folder + "/" + subdir)
         dir_cnt += 1
     return dir_cnt, file_cnt
+
 
 def img_split(dirs: list, destination: str, ratio = [0.8, 0.1, 0.1]):
     
@@ -68,13 +89,22 @@ def img_split(dirs: list, destination: str, ratio = [0.8, 0.1, 0.1]):
     for folder in ['train', 'validation', 'test']:
         if not os.path.exists(destination + '/' + folder):
             os.makedirs(destination + '/' + folder)
+            print(f"The {folder} dir doesn't exist, so it has been created, successfully in the data dir!")
         else:
-            for files in os.listdir(destination + '/' + folder):
-                path = os.path.join(destination + '/' + folder, files)
+            for subdir in os.listdir(destination + '/' + folder):
+                path = os.path.join(destination + '/' + folder, subdir)
+                #print(f"path {path}")
                 try:
                     shutil.rmtree(path)
                 except OSError:
-                    os.remove(path)
+                    try:
+                        for files in os.listdir(destination + '/' + folder + '/' + subdir):
+                            path = os.path.join(destination + '/' + folder + '/' + subdir, files)
+                            os.rmdir(path)
+                    except OSError:
+                        os.remove(path)
+            print(f"The {folder} dir did exist, so it has been removed")
+
 
     if len(val) > 0: #not empty
         train_dir_cnt, train_file_cnt = img_copy(dirs= train, destination = destination, folder = 'train')
@@ -88,9 +118,9 @@ def img_split(dirs: list, destination: str, ratio = [0.8, 0.1, 0.1]):
     print(f"& able to move {test_dir_cnt} folders & {test_file_cnt} files into TEST")
 
 if __name__ == "__main__":
-    dirs = getDirectoryList('../data/images/')
+    dirs = getDirectoryList('./data/images/')
     #print(dirs)
-    dest = "../data"
+    dest = "./data"
     ratio = [0.8, 0.1, 0.1]
 
     img_split(dirs, dest, ratio)
