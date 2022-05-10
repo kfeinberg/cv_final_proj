97aac4e366505024cbee76aa73744d3eb1c5519d
Kalissa Feinberg <kfeinberg@wustl.edu>
2022-05-09 22:20:19 -0400

    main

--- code/main.py
+++ code/main.py
@@ -1,2 +1,16 @@
 #TODO: iterate through images, for each pair of image use algos to calculate speed
 #TODO: maybe returns all the images with the speed labelled on them?
+#TODO: maybe have command line arguments? if they input just one directory 
+# we do physics way
+# and if we they do 2 directories then we do camera projection algo way
+directory = '/Users/kallifeinberg/Documents/Senior/Semester2/CS1430/cv_final_proj/data/train/annotations' # iterates through every file in this directory
+
+def parse_xml_files():
+    print("hello")
+
+
+def main():
+    parse_xml_files()
+
+if __name__ == '__main__':
+    main()
